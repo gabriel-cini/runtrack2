9299dadfb60df8b9768d86b12b0da73a6c59b9f7
gabriel-cini <115150782+gabriel-cini@users.noreply.github.com>
2023-05-10 23:27:20 +0200

    ajout jour 2

---
+++ jour02/job05/index.php
@@ -0,0 +1,24 @@
+<?php
+
+function affichePremier($nombre) {
+    if ($nombre < 2) {
+        return false;
+    }
+
+    for ($i = 2; $i <= sqrt($nombre); $i++) {
+        if ($nombre % $i === 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+for ($nombre = 2; $nombre <= 1000; $nombre++) {
+    if (affichePremier($nombre)) {
+        echo $nombre . "<br />";
+    }
+}
+
+?>
+
